6848dadc6f75f68f1917edf6af7bf6bde1a0dae1
Luke <lcp77@cornell.edu>
2020-05-14 12:51:16 -0400

    Luke: fixed import bug in control

--- testing/control.py
+++ testing/control.py
@@ -6,7 +6,7 @@ from multiprocessing import Pool
 scripts = ('wifi_data_collection.py', 'lsm9ds1_data_collection.py')
 
 def run_script(script):
-    os.system('python {}'.format(script))
+    os.system('python3 {}'.format(script))
 
 pool = Pool(2)
 pool.map(run_script, scripts)
